24e26d1457bbe60365bbbe80ec1ed4f69564eeff
Fida Waseque Choudhury <fida.waseque@gmail.com>
2026-06-03 12:58:32 +0600

    📦 chore(client): remove nitro bff and server-side dependencies

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,50 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "@nuxt/eslint",
-    "@nuxt/ui",
-    "@comark/nuxt",
-    "@nuxthub/core",
-    "nuxt-auth-utils",
-    "nuxt-charts",
-    "nuxt-csurf",
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@comark/nuxt',
+    'nuxt-auth-utils',
+    'nuxt-charts',
+    'nuxt-csurf'
   ],
 
   devtools: {
-    enabled: true,
+    enabled: true
   },
 
-  css: ["~/assets/css/main.css"],
+  css: ['~/assets/css/main.css'],
 
   experimental: {
-    viewTransition: true,
+    viewTransition: true
   },
 
-  compatibilityDate: "2024-07-11",
-
-  nitro: {
-    experimental: {
-      openAPI: true,
-    },
-  },
-
-  hub: {
-    db: "sqlite",
-    blob: true,
-  },
+  compatibilityDate: '2024-07-11',
 
   vite: {
     optimizeDeps: {
-      include: ["striptags"],
-    },
+      include: ['striptags']
+    }
   },
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: "never",
-        braceStyle: "1tbs",
-      },
-    },
-  },
-});
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
